c0c6bcb65ffa1ba055cadfaad459f97c35ac9b04
Gibran Tavares <gibran@edu.unifor.br>
2018-06-24 23:02:18 -0300

    Configurando travis ci

--- services/cache.js
+++ services/cache.js
@@ -1,20 +1,20 @@
 const mongoose = require('mongoose');
 const redis = require('redis');
 const util = require('util');
+const keys = require('../config/keys');
 
-const redisUrl = 'redis://127.0.0.1:6379';
-const client = redis.createClient(redisUrl);
-client.get = util.promisify(client.get);
+const client = redis.createClient(keys.redisUrl);
+client.hget = util.promisify(client.hget);
 const exec = mongoose.Query.prototype.exec;
 
 
-mongoose.Query.prototype.cache = function() {
+mongoose.Query.prototype.cache = function(options = {}) {
   this.useCache = true;
+  this.hashKey = JSON.stringify(options.key || '');
   return this;
 }
 
 mongoose.Query.prototype.exec = async function() {
-  
   if (!this.useCache) {
     const result = await exec.apply(this, arguments);
     return result;
@@ -24,7 +24,7 @@ mongoose.Query.prototype.exec = async function() {
     collection: this.mongooseCollection.name
   }));
 
-  const cacheValue = await client.get(key);
+  const cacheValue = await client.hget(this.hashKey, key);
 
   if (cacheValue) {
     const doc = JSON.parse(cacheValue);
@@ -36,7 +36,13 @@ mongoose.Query.prototype.exec = async function() {
 
   const result = await exec.apply(this, arguments);
 
-  client.set(key, JSON.stringify(result), 'EX', 10);
+  client.hset(this.hashKey, key, JSON.stringify(result), 'EX', 10);
 
   return result;
 }
+
+module.exports = {
+  clearHash(hashKey) {
+    client.del(JSON.stringify(hashKey))
+  }
+};
